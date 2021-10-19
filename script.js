07af95ffe2ca69ffab41b1fad26c7b08fa3462b2
Arthur Santos <arthur_bsggg@hotmail.com>
2021-10-19 00:02:58 -0300

    card oculto recebendo valores - eventos de mostrar o card oculto refeitos e adicionados - botão de visualizar cards adicionado - base do data-set do card oculto adicionado (criar lógica)

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var idcards = 0;
 AddCategoria("12", "Pegar o carregador");
-AddCategoria("12", "Pegar o carregador");
-AddCategoria("12", "Pegar o carregador");
-AddCategoria("12", "Pegar o carregador");
+AddCategoria("3", "Treinão");
+AddCategoria("12", "Trabalho ingles");
+
+
 
 
 /* Função chamada quando vamos adicionar nova categoria */
@@ -45,9 +46,9 @@ function AddCategoria(dias, descrição) {
             </div>
 
             <div class="div-botoes">
-                <button class="button-icon" id="button-alteraTask"> <i class="far fa-edit"></i> </button>
+                <button class="button-icon" id="button-visualizaTask"> <i class="far fa-eye"></i> </button>
 
-                <button class="button-icon" id="button-DeletaTask"> <i class="far fa-times-circle"></i> </button>
+                <button class="button-icon" id="button-DeletaTask"> <i class="far fa-trash-alt"></i> </button>
             </div>
         </div>
 
@@ -56,42 +57,95 @@ function AddCategoria(dias, descrição) {
         </div>
     `;
 
+
   /* Teve que dar esse role todo mesmo, não pensei em nada melhor. Se alterar alguma coisa dentro da estrutura da div mais pra frente, de um log nesse newcard que consiste na div do card e veja o path para chegar no botão de novo e adicionar os eventos e os datasets */
 
+
   /* Adiciona o evento de clique nos dois botões */
-  newcard.children[0].children[1].children[0].addEventListener("click", modificaTarefa);
-  newcard.children[0].children[1].children[1].addEventListener("click", modificaTarefa);
+  let botaovisualizar = newcard.children[0].children[1].children[0];
+  let botaodeletar = newcard.children[0].children[1].children[1]
+
+
+  botaovisualizar.addEventListener("click", modificaTarefa);
+  botaodeletar.addEventListener("click", modificaTarefa);
+
 
   /* Adiciona um atributo data para identificarmos os cards com os botões para modificar depois */
-
   /* Atribuindo os data-attributes nos botoes */
-  newcard.children[0].children[1].children[0].setAttribute("data-idcard", idcards);
-  newcard.children[0].children[1].children[1].setAttribute("data-idcard", idcards);
 
+  botaovisualizar.setAttribute("data-idcard", idcards);
+  botaodeletar.setAttribute("data-idcard", idcards);
 
 
   /* Atribuindo no conteiner do Card */
   newcard.setAttribute("data-idcard", idcards);
 
+
   /* Atribue no ID card para não se repetir*/
   idcards++;
+
+
   /* Muda a cor de fundo do Card */
   newcard.style.backgroundColor = coresaleatorias();
+
 
   /* Adiciona o card criado dentro do container */
   let containerCards = document.querySelector(".container-cards");
   containerCards.append(newcard);
 
 
-  /* Area para testes do clique no card*/
-  /* Caso clique nos dias ou no conteiner da descrição ativa um evento.*/
+  /* Evento adicionado para quando clicarmos no card dias e titulos areas */
+  newcard.addEventListener("click", ApresentaCard);
 
-  newcard.children[0].children[0].children[0].addEventListener("click", ApresentaCard);
-  newcard.children[1].addEventListener("click", ApresentaCard);
+}
 
-  function ApresentaCard(){
-    console.log("Abrir Tarefa")
-  }
+function ApresentaCard(event){
+  /* Colocar eventos em um botão do card para sumir com o card*/
+  let conteinerCardOcultodaViladaFolha = document.querySelector(".conteiner-card-expandido");
+  
+
+   if(conteinerCardOcultodaViladaFolha.style.display == "none"){
+     conteinerCardOcultodaViladaFolha.style.display = "flex";
+   }
+
+
+  /* separa o idcard em todos os cantos do conteiner, pois cada elemento que clicamos o objeto de eventos vem com um pathing diferente. Usa a quantidade de pathing para reparar, pois cada um tem um diferente */
+
+  let idcard = '';
+  
+   if(event.path.length == 9){
+    idcard = event.path[1].dataset.idcard;
+
+   } else
+   
+   if(event.path.length == 10){
+     idcard = event.path[2].dataset.idcard;
+
+   } else
+
+   if(event.path.length == 11){
+     idcard = event.path[3].dataset.idcard;
+  
+   } else
+
+   if(event.path.length == 12){
+     idcard = event.path[4].dataset.idcard;
+  
+   }
+
+
+  /* busca o elemento pelo data-set */
+  let idcardDiv = document.querySelector(`[data-idcard="${idcard}"]`);
+  
+
+  /* Separa os valores em variáveis */
+  let tempo = idcardDiv.childNodes[1].children[0].innerText;
+  let titulo = idcardDiv.childNodes[3].children[0].innerText;
+  
+
+  /* Coloca no Card oculto */
+  conteinerCardOcultodaViladaFolha.children[0].children[0].innerHTML = `<h2 id="titulo-header-card">${titulo} em ${tempo}</h2>`;
+
 }
 
 
@@ -101,42 +155,61 @@ function AddCategoria(dias, descrição) {
 function modificaTarefa(event) {
   /* valor do dataset para fazer a condicional */
   let valorid = event.path[1].getAttribute("id");
+
+
   /* valor do dataset do botão do evento atual */
   idcardBotao = event.path[1].dataset.idcard;
+
+
   /* valor do dataset da div */ /* usamos o dataset do botão para localizar o card, por isso que adicionamos o data set no momento da criação da tarefa */
   idcardDiv = document.querySelector(`[data-idcard="${idcardBotao}"]`);
 
-  if (valorid == "button-alteraTask") {
-    console.log("Alterou");
+
+  /* Vê qual botão é e toma uma ação */
+  if (valorid == "button-visualizaTask") {
+    ApresentaCard(event);
 
   } else 
-  
   
   if (valorid == "button-DeletaTask") {
     idcardDiv.remove();
   }
+
 }
+
+
 
 /* Atribuindo o evento no forms de adicionar tarefas. */
 let formscadastro = document.querySelector("#formscadastro");
 formscadastro.addEventListener("submit", cadastrarTarefa);
 
+
+
+/* Função que pega os valores dos forms e cria um card */
 function cadastrarTarefa(event) {
   event.preventDefault();
+
 
   /* Joga na função vista acima de criar os Cards os valores dos inputs */
   /* event.target[].value representa os valores dos inputs no momento do evento. Ele referencia a ordem que os inputs aparecem no conteiner do evento, no caso o forms. Atualmente, o de dias é o primeiro, e o segundo é o de descrição. */
   AddCategoria(event.target[0].value, event.target[1].value);
 
+
   /* Reseta os valores dos inputs p/ o user não ter que apagar */
   event.target[0].value = "";
   event.target[1].value = "";
+
 }
 
+
+
+/* Função de cores aleatorias para os cards*/
 function coresaleatorias() {
   var r = Math.random() * 255;
   var g = Math.random() * 255;
   var b = Math.random() * 255;
+  
 
   return `rgb(${r}, ${g}, ${b})`;
+
 }
